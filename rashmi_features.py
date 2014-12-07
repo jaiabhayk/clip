cf34e3d008c07f968f4e9bc99b24b6c65ee5572e
jaiabhayk <jai.abhayk@gmail.com>
2014-12-07 00:52:32 -0500

    updated data files

--- rashmi_features.py
+++ rashmi_features.py
@@ -3,6 +3,6 @@ from tweet import *
 def getRashmiFeatures(tweet_content):
     f_list = []
 
-    f_list.append(Feature("num_words2", len(tweet_content)))
+#     f_list.append(Feature("num_words2", len(tweet_content)))
 #     f_list += f_list1
     return f_list
