bd321531b1684b887e29ed83f3675b124c96155e
yogarshi <yogarshi.vyas@gmail.com>
2014-12-06 21:35:15 -0500

    Updated to support print for Feature class.

---
+++ mixed_bag.py
@@ -0,0 +1,71 @@
+__author__ = 'yogarshi'
+"""
+Mixed bag of features
+"""
+
+from tweet import *
+
+def num_capital(tweet_content):
+
+    val = 0
+    for each_token in tweet_content:
+        if each_token.isupper():
+            val += 1
+
+    return [Feature("num_capital", val)]
+
+
+def add_bigrams(tweet_content):
+
+    f_list = []
+    bi_dict = {}
+    for i in range(len(tweet_content)-1):
+        if tweet_content[i].isalnum() and tweet_content[i+1].isalnum():
+            t = tweet_content[i].lower() + '_' + tweet_content[i+1].lower()
+            if t not in bi_dict:
+                bi_dict[t] = 0
+            bi_dict[t] +=1
+
+    for each in bi_dict:
+        f_list.append(Feature(each, bi_dict[each]))
+    return f_list
+
+
+def add_trigrams(tweet_content):
+
+    f_list = []
+    tri_dict = {}
+    for i in range(len(tweet_content)-2):
+        if tweet_content[i].isalnum() and tweet_content[i+1].isalnum() and tweet_content[i+2].isalnum():
+            t = tweet_content[i].lower() + '_' + tweet_content[i+1].lower() + '_' + tweet_content[i+2]
+            if t not in tri_dict:
+                tri_dict[t] = 0
+            tri_dict[t] += 1
+
+    for each in tri_dict:
+        f_list.append(Feature(each, tri_dict[each]))
+    return f_list
+
+def is_retweet(tweet_content):
+
+    if tweet_content[0] == "RT":
+        return [Feature("is_retweet", 1)]
+    else:
+        return [Feature("is_retweet", 0)]
+
+def combine_features(tweet_content):
+
+    f_list = []
+
+    f_list += num_capital(tweet_content)
+    f_list += add_bigrams(tweet_content)
+    f_list += add_trigrams(tweet_content)
+    f_list += is_retweet(tweet_content)
+
+
+    return f_list
+
+
+
+
+
--- yogarshi_features.py
+++ yogarshi_features.py
@@ -1,14 +1,10 @@
 from tweet import *
+import mixed_bag
+
 
 def getYogarshiFeatures(tweet_content):
     f_list = []
 
-    f_list.append(Feature("num_words1", len(tweet_content)))
-#     #feature_list += sample_feature(tweet_content)
-#     feature_list += sample_feature2(tweet_content)
-#     #feature_list += num_words(tweet_content)
-#     feature_list += add_bigrams(tweet_content)
-#     feature_list += add_trigrams(tweet_content)
-    
-#     f_list += f_list1
+    f_list += mixed_bag.combine_features(tweet_content)
+
     return f_list
