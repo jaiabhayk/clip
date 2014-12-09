312dffc45f12776c8298d7b6a6dd0f487e9cecac
Rashmi Sankepally <rashmisankepally@users.noreply.github.com>
2014-12-09 13:48:48 -0500

    Changed feature names

--- symbolic_features.py
+++ symbolic_features.py
@@ -7,40 +7,40 @@ def smiley_count(tweet_content):
     smiley_sad = [':(', ':-(']
     smiley_kidding = [':P', ':-P']
     smiley_wink = [';)',';-)']
-    surprize = [':O',':-O']
-    laugh = [':D', ':-D',":'D",":'-D"]
-    cry = [":'(",":'-(", ":*(", ":*-(",":{",":["]
-    smilecry = [":')",":'-)"]
-    heart = ["<3"]
-    kiss = [":*",":X",":-*",":-X"]
-    indifference = [":|",":-|", "-_-"]
+    smiley_surprize = [':O',':-O']
+    smiley_laugh = [':D', ':-D',":'D",":'-D"]
+    smiley_cry = [":'(",":'-(", ":*(", ":*-(",":{",":["]
+    smiley_smilecry = [":')",":'-)"]
+    smiley_heart = ["<3"]
+    smiley_kiss = [":*",":X",":-*",":-X"]
+    smiley_indifference = [":|",":-|", "-_-"]
     count = 0
-    smiley_dict={"smile":0,"sad":0,"kidding":0, "wink":0, "surprize":0,"laugh":0,"cry":0,"smilecry":0,"heart":0,"kiss":0,"indifference":0}
+    smiley_dict={"smiley_smile":0,"smiley_sad":0,"smiley_kidding":0, "smiley_wink":0, "smiley_surprize":0,"smiley_laugh":0,"smiley_cry":0,"smiley_smilecry":0,"smiley_heart":0,"smiley_kiss":0,"smiley_indifference":0}
  
     f_list=[]
     for each_token in tweet_content:
-        if each_token.upper() in smile:
-            smiley_dict["smile"]+=1
-        if each_token.upper() in sad:
-            smiley_dict["sad"]+=1
-        if each_token.upper() in kidding:
-            smiley_dict["kidding"]+=1
-        if each_token.upper() in wink:
-            smiley_dict["wink"]+=1
-        if each_token.upper() in surprize:
-            smiley_dict["surprize"]+=1
-        if each_token.upper() in laugh:
-            smiley_dict["laugh"]+=1
-        if each_token.upper() in cry:
-            smiley_dict["cry"]+=1
-        if each_token.upper() in smilecry:
-            smiley_dict["smilecry"]+=1
-        if each_token.upper() in heart:
-            smiley_dict["heart"]+=1
-        if each_token.upper() in kiss:
-            smiley_dict["kiss"]+=1
-        if each_token.upper() in indifference:
-            smiley_dict["indifference"]+=1
+        if each_token.upper() in smiley_smile:
+            smiley_dict["smiley_smile"]+=1
+        if each_token.upper() in smiley_sad:
+            smiley_dict["smiley_sad"]+=1
+        if each_token.upper() in smiley_kidding:
+            smiley_dict["smiley_kidding"]+=1
+        if each_token.upper() in smiley_wink:
+            smiley_dict["smiley_wink"]+=1
+        if each_token.upper() in smiley_surprize:
+            smiley_dict["smiley_surprize"]+=1
+        if each_token.upper() in smiley_laugh:
+            smiley_dict["smiley_laugh"]+=1
+        if each_token.upper() in smiley_cry:
+            smiley_dict["smiley_cry"]+=1
+        if each_token.upper() in smiley_smilecry:
+            smiley_dict["smiley_smilecry"]+=1
+        if each_token.upper() in smiley_heart:
+            smiley_dict["smiley_heart"]+=1
+        if each_token.upper() in smiley_kiss:
+            smiley_dict["smiley_kiss"]+=1
+        if each_token.upper() in smiley_indifference:
+            smiley_dict["smiley_indifference"]+=1
 
     for each_item in smiley_dict:
         f_list.append(Feature(each_item,smiley_dict[each_item]))  
