9b487ce1d701e6e22630a4f93aec0f89e2fe9281
jaiabhayk <jai.abhayk@gmail.com>
2014-12-09 23:45:10 -0500

    minor modificaitons

--- abhay_features.py
+++ abhay_features.py
@@ -5,5 +5,5 @@ from senti_wordnet_features import *
 def getAbhayFeatures(tweet):
     f_list = []
     f_list += getHashTagFeatures(tweet.tokenized)
-    f_list += getSentiScoreFeatures(tweet.tokenized)
+    f_list += getSentiScoreFeatures(tweet)
     return f_list
--- senti_wordnet_features.py
+++ senti_wordnet_features.py
@@ -4,8 +4,12 @@ from twokenize import *
 from vw_interface import *
 from tweet import *
 from collections import Counter
+from nltk.stem.porter import *
+stemmer = PorterStemmer()
 
 debug = False
+
+
 
 def populate_senti_word_dict(filename):
     """
@@ -29,35 +33,69 @@ def populate_senti_word_dict(filename):
 
 senti_word_dict = populate_senti_word_dict('senti_wordnet_dictionary.txt')
 
-def getSentiScoreFeatures(tweet_content):
-    if debug:print '\ntweet\n', ' '.join(tweet_content), '\n'
+def getSentiScoreFeatures(tweet):
+    if debug:print '\ntweet\n', ' '.join(tweet.tokenized), '\n'
     f_list = []
     scores = Counter()
-    pos_map = getPOStag(tweet_content)
-    for token in tweet_content:
-        pos = pos_map[token]
-        key = ''.join([token.lower(), "#", pos])
+    no_of_sentences = 0
+    sent_score_count = 0
+    
+    phrases = ['as_long_as_', '_as_long_as_', 'like_the_', '_like_the_', 'so_to_speak_', 
+               '_so_to_speak', '_so_to_speak_', 'so_', '_so_', '_since_', '_more_and_more_', '_more_than_'
+               , '_all_of_a_sudden_','all_of_a_sudden_',]
+    
+    phrases_sentences_map = {};
+    tokenized_content = ('_'.join(tweet.tokenized)).lower()
+    if debug:print tokenized_content
+    phrase_found = 'sentence'
+    for phrase in phrases:
+        if phrase in tokenized_content:
+            phrase_found = phrase
+            break
+    if debug:print 'phrase found:-', phrase_found,'\n'
+    
+    for i in range(len(tweet.tokenized)):
+        pos = (tweet.posTags.name[i]).lower()
+        token = (tweet.tokenized[i]).lower()
+        token = stemmer.stem(token.decode('utf-8')).encode('utf-8')
+        key = ''.join([token, "#", pos])
         # TODO don't consider stop words or a, as the etc ??
         # TODO use the lemma or stemmed word for score ??
         # first clean/normalize the words (ex:- you're etc)
         #TODO Apply on bigram as well or rather apply on phrases
-        if key not in senti_word_dict:
-            if debug:print 'No corresponding score found in the dictionary, skipping key=' , key
+        
+        if pos in {',', '&','!','#', '@' }:
+#             if debug: print 'Found end of sentence/phrase, key=',key
+            if sent_score_count!=0:
+                no_of_sentences+=1
+                scores[ ''.join([phrase_found,'_',str(no_of_sentences)])] = sent_score_count
+            sent_score_count = 0
             continue
-        if debug:print '\nkey=', key, ':score=', senti_word_dict[key]
-        score = senti_word_dict[key]
-        if (score >0.1):
-#             scores['total_max_pos_senti_score'] += score
-            if (score not in scores) or (score > scores['max_pos_senti_score']):
-                scores['max_pos_senti_score'] = score
-        elif score<-0.1:
-#             scores['total_max__neg_senti_score']=score
-            if (score not in scores) or (score < scores['max_neg_senti_score']):
-                scores['max_neg_senti_score'] = score
             
-#         scores[key] += score
+        if key not in senti_word_dict:
+            #TODO if not found then fallback to lemma or stemmed word
+#             if debug:print 'No corresponding score found in the dictionary, skipping key=' , key
+            continue
+#         if debug:print '\nkey=', key, ':score=', senti_word_dict[key]
+        score = senti_word_dict[key]
+        sent_score_count+=score
+
+#         if (score >0):
+#             scores['total_pos_senti_score'] += score
+# #             if (score not in scores) or (score > scores['max_pos_senti_score']):
+# #                 scores['max_pos_senti_score'] = score
+#         elif score<0:
+#             scores['total_neg_senti_score']+=score
+# #             if (score not in scores) or (score < scores['max_neg_senti_score']):
+# #                 scores['max_neg_senti_score'] = score
+#             
+#         scores[key] = score
 #         scores['total_senti_score'] += score
+
+
      
+    if debug: print '\n token:-', tweet.tokenized 
+    if debug: print '\n pos:-', tweet.posTags
     if debug:print '\n All senti-word scores:- \n', scores
     for tag in scores:
         f_list.append(Feature(tag, scores[tag]))
@@ -70,28 +108,6 @@ def getSentiScoreFeatures(tweet_content):
 
 
 
-def getPOStag(content):
-    """ TODO
-    Implement this to get the POS tag of the word (noun, verb etc.)
-    """ 
-    pos_map = {};
-    possible_pos = ['n', 'v','a',  'r']
-    
-    for token in content:
-        # TODO remove this work-around to know the pos
-        # default is noun, n stands for noun (similar to wordnet 3.0
-        
-        pos_map[token] = 'n'  
-        for pos in possible_pos:
-            key = ''.join([token.lower(), "#", pos])
-            if key not in senti_word_dict:
-#                 if debug:print 'pos tagger, key NOT found in senti_word_dict-',key
-                continue
-            else:
-#                 if debug:print 'pos tagger, key found in senti_word_dict, exit-',key 
-                pos_map[token] = pos  
-                break;
-    return pos_map
 
 if __name__ == '__main__':
     pos = [];
