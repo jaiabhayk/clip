c8d4baa4e735122e2955d895792678af0e145c97
rashmi <rashmi.sankepally@gmail.com>
2014-12-16 01:18:05 -0500

    added dependency path features

--- rashmi_features.py
+++ rashmi_features.py
@@ -9,6 +9,7 @@ def getRashmiFeatures(tweet):
     f_list += symbolic_features.smiley_count(tweet.tokenized)
     f_list += punctuations.punctuation_count(tweet.tokenized)
     f_list += dependency_features.root_count(tweet.dep_parse)
+    f_list += dependency_features.dependency_path(tweet.dep_parse)
 #     f_list.append(Feature("num_words2", len(tweet_content)))
 #     f_list += f_list1
     return f_list
